d873dc201681f0f85e5a9aa4eafde5dabd343373
haidasse <j.haidasse@upm.ac.ma>
2024-05-20 10:03:11 +0100

    firstoneF

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'nav',component:NavBarComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
